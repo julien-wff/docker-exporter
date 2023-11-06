02205d4d96eea7c5bdbd2d6ae99ef9d6de2d36e5
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-11-06 20:56:37 +0000

    feat: scrape duration metric

--- internal/collectors/newCollector.go
+++ internal/collectors/newCollector.go
@@ -3,10 +3,12 @@ package collectors
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"sync"
+	"time"
 )
 
 type DockerCollector struct {
 	networkContainersMetrics *prometheus.Desc
+	scrapeDurationMetric     *prometheus.Desc
 }
 
 func (collector *DockerCollector) Describe(ch chan<- *prometheus.Desc) {
@@ -16,8 +18,13 @@ func (collector *DockerCollector) Describe(ch chan<- *prometheus.Desc) {
 func (collector *DockerCollector) Collect(ch chan<- prometheus.Metric) {
 	var wg sync.WaitGroup
 	wg.Add(1)
+
 	go CollectNetworkContainers(&wg, collector, ch)
+
+	start := time.Now()
 	wg.Wait()
+
+	ch <- prometheus.MustNewConstMetric(collector.scrapeDurationMetric, prometheus.GaugeValue, time.Since(start).Seconds())
 }
 
 func NewDockerCollector() *DockerCollector {
@@ -26,6 +33,12 @@ func NewDockerCollector() *DockerCollector {
 			"docker_network_container_count",
 			"Number of containers per network",
 			[]string{"name"},
+			nil,
+		),
+		scrapeDurationMetric: prometheus.NewDesc(
+			"docker_last_scrape_duration_seconds",
+			"Time it took to scrape docker metrics",
+			nil,
 			nil,
 		),
 	}
