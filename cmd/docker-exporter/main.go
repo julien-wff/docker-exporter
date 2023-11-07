eac13a7cd412942e2c6c0bac992b5952e6391779
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-11-07 21:04:34 +0000

    feat: print config on startup

--- cmd/docker-exporter/main.go
+++ cmd/docker-exporter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/julien-wff/docker-exporter/internal/collectors"
+	"github.com/julien-wff/docker-exporter/internal/config"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
@@ -17,6 +18,13 @@ func main() {
 	mux := http.NewServeMux()
 	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 	mux.Handle("/metrics", promHandler)
+
+	cfg := config.GetConfig()
+	log.Printf("----- Docker Exporter -----\n")
+	log.Println("")
+	log.Println("Config:")
+	log.Printf("- Using a request timeout of %s\n", cfg.RequestTimeout.String())
+	log.Println("")
 
 	log.Printf("Starting server on port 9100\n")
 	log.Fatal(http.ListenAndServe(":9100", mux))
